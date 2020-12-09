d6a673863fbf180bc09f387ff9207a419bb35d64
Wup-Wup <55537968+Wup-Wup@users.noreply.github.com>
2020-12-09 09:16:32 +0100

    Add files via upload

--- Aktien/src/TestMain.java
+++ Aktien/src/TestMain.java
@@ -2,7 +2,16 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -16,8 +25,17 @@ public class TestMain {
 		
 		System.out.println();
 		test();
+		connect();
+		createNewTable();
+		
+		for(int i=0;i<dates.size();i++) {
+			insert(i);
+		}
+		selectAll();
 	}
 	
+	static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd LLLL yyyy");
+	static String urld;
 	static ArrayList<LocalDate> dates = new ArrayList<LocalDate>();
 	static ArrayList <Double> wert = new ArrayList <Double>();
 	static String s;
@@ -25,8 +43,8 @@ public class TestMain {
 	
 	public static void test() throws MalformedURLException, JSONException, IOException {
 		 
-		System.out.println("Bitte geben sie eine der folgenden Aktienkurse ein: Tsla(tesla), Ap(Apple)");
-		
+		System.out.println("Bitte geben sie eine der folgenden Aktienkurse ein: Tsla(Tesla), Ap(Apple)");
+
 		Scanner gustav = new Scanner(System.in);
 		s=gustav.next();
 		
@@ -42,8 +60,90 @@ public class TestMain {
 	           System.out.println(wert.get(i));
 	       }
 	}
+	
+	public static void connect() {
+		Connection conn = null;
+		try {
+			// db parameters
+			urld = "jdbc:sqlite:C:\\Users\\Alexander Bertoni\\Documents\\Schule\\3.Klasse\\Programmieren\\Programme\\Aktien\\Datenbank.db";
+			// create a connection to the database
+			conn = DriverManager.getConnection(urld);
 
-}
+			System.out.println("Connection to SQLite has been established.");
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException ex) {
+				System.out.println(ex.getMessage());
+			}
+		}
+	}
+		
+		public static void createNewTable() {
+			// SQL statement for creating a new table
+			String sql = "CREATE TABLE IF NOT EXISTS "+s+"(" + "Datum date,\n" + "Wert decimal);";
+			
+			try {
+				Connection conn = DriverManager.getConnection(urld);
+				Statement stmt = conn.createStatement();
+				stmt.execute(sql);
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+
+			}
+		}
+		
+		private static Connection connection() {
+			Connection conn = null;
+			try {
+				conn = DriverManager.getConnection(urld);
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			}
+			return conn;
+		}
+		
+		public static void insert(int i) {
+			String sql = "INSERT INTO "+s+"(Datum,Wert) VALUES(?,?)";
+
+			try {
+
+				Connection conn = connection();
+
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+
+			    pstmt.setString(1, dates.get(i).format(formatter));
+				pstmt.setDouble(2,wert.get(i));
+				pstmt.executeUpdate();
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+				System.out.println("Hier");
+			}
+		}
+		
+		public static void selectAll() {
+			String sql = "SELECT * FROM "+s;
+
+			try {
+				Connection conn = connection();
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(sql);
+
+				while (rs.next()) {
+					System.out.println("Datum: " + rs.getString("Datum")
+							+ "\nWert: " + rs.getDouble("Wert")+"\n");
+				}
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+				System.out.println("proof");
+			}
+		}
+	}
 
 
 	
